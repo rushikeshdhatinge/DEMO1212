969cee62f6d0a45367e3fae2987b3145651a2c0d
Rushikesh0527 <rushidhatinge@gmail.com>
2026-03-20 23:53:52 +0530

    bug fixed

--- Notes/routes/api.js
+++ Notes/routes/api.js
@@ -1,5 +1,6 @@
 import express from "express";
 import db from "../db.js";
+import { Result } from "postcss";
 
 const router = express.Router();
 let today = new String(new Date(Date.now())).slice(4, 15); // "Sep 11 2023";
@@ -13,9 +14,25 @@ router.get("/posts", (req, res) => {
   res.send("Posts API");
 });
 
+
+router.get("/post/:Id",async(req,res)=>{
+  let Id = req.params.Id;  
+
+
+
+  try {
+    const result = await db.query("SELECT * FROM notes WHERE id=$1",[Id]);
+  res.render("note",{note :result.row[0]})
+
+  } catch (error) {
+    console.error(error);
+  }
+})
+
 router.post("/post",(req,res)=>{
- let content = "Nothing"
-let title = "Rushikesh"
+ let content = req.body.content;
+let title = req.body.title;
+db.query("INSERT INTO notes VALUES()")
  res.render("index",{content,title})
 });
 
